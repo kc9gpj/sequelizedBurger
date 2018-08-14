5c76d261152e00d0b1c1903ac61de1659918237b
david  hoffmann <davidhoffmann@davids-MacBook-Pro.local>
2018-08-13 19:22:17 -0500

    none

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name, devoured) VALUES ('Rodeo King', false);
+INSERT INTO burgers (name, devoured) VALUES ('Big Mac', false);
+INSERT INTO burgers (name, devoured) VALUES ('Big Kahuna Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Whopper', false);
