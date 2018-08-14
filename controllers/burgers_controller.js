4fd2237aab8d8406bcd47735f70da27c9d4827fd
david  hoffmann <davidhoffmann@davids-MacBook-Pro.local>
2018-08-13 20:02:51 -0500

    error fixes

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,6 @@
+var express = require("express");
+var cat = require("../models/burger.js");
+
+
+
+// 4. Create the `router` for the app, and export the `router` at the end of your file.
---
+++ models/burger.js
@@ -0,0 +1,35 @@
+var orm = require("../config/orm.js");
+
+
+
+
+// Inside your `burger` directory, create a folder named `models`.
+
+// * In `models`, make a `burger.js` file.
+
+//   * Inside `burger.js`, import `orm.js` into `burger.js`
+
+//   * Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
+
+//   * Export at the end of the `burger.js` file.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = cat;
