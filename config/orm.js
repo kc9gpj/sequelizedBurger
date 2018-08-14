28c80c6603d455a524709b7703bd7617f555c010
david  hoffmann <davidhoffmann@davids-MacBook-Pro.local>
2018-08-14 15:54:32 -0500

    none

--- config/orm.js
+++ config/orm.js
@@ -8,29 +8,15 @@ var connection = require("../config/connection.js");
 // * `updateOne()`
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var orm = {
+    selectAll: function() {
+      var queryString = "SELECT * FROM burgers";
+      connection.query(queryString, function(err, result) {
+        if (err) throw err;
+        console.log(result);
+      });
+    },
+ };
 
 
 module.exports = orm;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,6 +1,19 @@
 var express = require("express");
-var cat = require("../models/burger.js");
+var burgers = require("../models/burger.js");
 
+var router = express.Router();
 
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function(req, res) {
+  burgers.all(function(data) {
+    var hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
 
-// 4. Create the `router` for the app, and export the `router` at the end of your file.
+});
+
+// Export routes for server.js to use.
+module.exports = router;
