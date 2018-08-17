74a58103e3eb7ae6aac4b58cd883729ac95c4b74
David Hoffmann <kc9gpj12@gmail.com>
2018-08-17 18:37:35 -0500

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+  host: "blonze2d5mrbmcgf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "root",
-  password: "root",
-  database: "burgers_db"
+  user: "dlnrdovtboolo7yo",
+  password: "iph5e2u3f4emj4lf",
+  database: "fwfn7bo9qdjxr5ie"
 });
 
 // Make connection.
