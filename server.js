6805ab4bd33ca71c01cad0bb3be47aebdf62e8d1
david  hoffmann <davidhoffmann@davids-MacBook-Pro.local>
2018-08-17 09:20:20 -0500

    none

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-var PORT = process.env.PORT || 8000;
-
 var app = express();
+
+var PORT = process.env.PORT || 8000;
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
